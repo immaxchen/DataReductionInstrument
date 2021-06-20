89b82555b0a13963adbc37818886de740faa7ea4
unknown <immaxchen@gmail.com>
2021-06-20 16:04:41 +0800

    first upload

---
+++ DataReductionInstrument/MainForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: USER
+ * Date: 2021/6/19
+ * Time: 上午 11:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace DataReductionInstrument
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button buttonOpen;
+		private System.Windows.Forms.Button buttonPreRow;
+		private System.Windows.Forms.Button buttonPreCol;
+		private System.Windows.Forms.Button buttonIncCol;
+		private System.Windows.Forms.Button buttonExcCol;
+		private System.Windows.Forms.Button buttonIncLbl;
+		private System.Windows.Forms.Button buttonExcLbl;
+		private System.Windows.Forms.Button buttonIncVal;
+		private System.Windows.Forms.Button buttonExcVal;
+		private System.Windows.Forms.Button buttonPlus;
+		private System.Windows.Forms.Button buttonDiff;
+		private System.Windows.Forms.Button buttonSave;
+		private System.Windows.Forms.CheckBox checkBoxCase;
+		private System.Windows.Forms.CheckBox checkBoxTranspose;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonOpen = new System.Windows.Forms.Button();
+			this.buttonPreRow = new System.Windows.Forms.Button();
+			this.buttonPreCol = new System.Windows.Forms.Button();
+			this.buttonIncCol = new System.Windows.Forms.Button();
+			this.buttonExcCol = new System.Windows.Forms.Button();
+			this.buttonIncLbl = new System.Windows.Forms.Button();
+			this.buttonExcLbl = new System.Windows.Forms.Button();
+			this.buttonIncVal = new System.Windows.Forms.Button();
+			this.buttonExcVal = new System.Windows.Forms.Button();
+			this.buttonPlus = new System.Windows.Forms.Button();
+			this.buttonDiff = new System.Windows.Forms.Button();
+			this.buttonSave = new System.Windows.Forms.Button();
+			this.checkBoxCase = new System.Windows.Forms.CheckBox();
+			this.checkBoxTranspose = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// buttonOpen
+			// 
+			this.buttonOpen.Location = new System.Drawing.Point(12, 12);
+			this.buttonOpen.Name = "buttonOpen";
+			this.buttonOpen.Size = new System.Drawing.Size(194, 42);
+			this.buttonOpen.TabIndex = 0;
+			this.buttonOpen.Text = "Open";
+			this.buttonOpen.UseVisualStyleBackColor = true;
+			this.buttonOpen.Click += new System.EventHandler(this.ButtonOpenClick);
+			// 
+			// buttonPreRow
+			// 
+			this.buttonPreRow.Location = new System.Drawing.Point(12, 82);
+			this.buttonPreRow.Name = "buttonPreRow";
+			this.buttonPreRow.Size = new System.Drawing.Size(30, 21);
+			this.buttonPreRow.TabIndex = 1;
+			this.buttonPreRow.Text = "👁";
+			this.buttonPreRow.UseVisualStyleBackColor = true;
+			this.buttonPreRow.Click += new System.EventHandler(this.ButtonPreRowClick);
+			// 
+			// buttonPreCol
+			// 
+			this.buttonPreCol.Location = new System.Drawing.Point(109, 61);
+			this.buttonPreCol.Name = "buttonPreCol";
+			this.buttonPreCol.Size = new System.Drawing.Size(30, 21);
+			this.buttonPreCol.TabIndex = 2;
+			this.buttonPreCol.Text = "👁";
+			this.buttonPreCol.UseVisualStyleBackColor = true;
+			this.buttonPreCol.Click += new System.EventHandler(this.ButtonPreColClick);
+			// 
+			// buttonIncCol
+			// 
+			this.buttonIncCol.Location = new System.Drawing.Point(146, 61);
+			this.buttonIncCol.Name = "buttonIncCol";
+			this.buttonIncCol.Size = new System.Drawing.Size(60, 42);
+			this.buttonIncCol.TabIndex = 3;
+			this.buttonIncCol.Text = "Inc. Col";
+			this.buttonIncCol.UseVisualStyleBackColor = true;
+			this.buttonIncCol.Click += new System.EventHandler(this.ButtonIncColClick);
+			// 
+			// buttonExcCol
+			// 
+			this.buttonExcCol.Location = new System.Drawing.Point(212, 61);
+			this.buttonExcCol.Name = "buttonExcCol";
+			this.buttonExcCol.Size = new System.Drawing.Size(60, 42);
+			this.buttonExcCol.TabIndex = 4;
+			this.buttonExcCol.Text = "Exc. Col";
+			this.buttonExcCol.UseVisualStyleBackColor = true;
+			this.buttonExcCol.Click += new System.EventHandler(this.ButtonExcColClick);
+			// 
+			// buttonIncLbl
+			// 
+			this.buttonIncLbl.Location = new System.Drawing.Point(13, 109);
+			this.buttonIncLbl.Name = "buttonIncLbl";
+			this.buttonIncLbl.Size = new System.Drawing.Size(60, 42);
+			this.buttonIncLbl.TabIndex = 5;
+			this.buttonIncLbl.Text = "Inc. Lbl";
+			this.buttonIncLbl.UseVisualStyleBackColor = true;
+			this.buttonIncLbl.Click += new System.EventHandler(this.ButtonIncLblClick);
+			// 
+			// buttonExcLbl
+			// 
+			this.buttonExcLbl.Location = new System.Drawing.Point(79, 109);
+			this.buttonExcLbl.Name = "buttonExcLbl";
+			this.buttonExcLbl.Size = new System.Drawing.Size(60, 42);
+			this.buttonExcLbl.TabIndex = 6;
+			this.buttonExcLbl.Text = "Exc. Lbl";
+			this.buttonExcLbl.UseVisualStyleBackColor = true;
+			this.buttonExcLbl.Click += new System.EventHandler(this.ButtonExcLblClick);
+			// 
+			// buttonIncVal
+			// 
+			this.buttonIncVal.Location = new System.Drawing.Point(13, 157);
+			this.buttonIncVal.Name = "buttonIncVal";
+			this.buttonIncVal.Size = new System.Drawing.Size(60, 42);
+			this.buttonIncVal.TabIndex = 7;
+			this.buttonIncVal.Text = "Inc. Val";
+			this.buttonIncVal.UseVisualStyleBackColor = true;
+			this.buttonIncVal.Click += new System.EventHandler(this.ButtonIncValClick);
+			// 
+			// buttonExcVal
+			// 
+			this.buttonExcVal.Location = new System.Drawing.Point(79, 157);
+			this.buttonExcVal.Name = "buttonExcVal";
+			this.buttonExcVal.Size = new System.Drawing.Size(60, 42);
+			this.buttonExcVal.TabIndex = 8;
+			this.buttonExcVal.Text = "Exc. Val";
+			this.buttonExcVal.UseVisualStyleBackColor = true;
+			this.buttonExcVal.Click += new System.EventHandler(this.ButtonExcValClick);
+			// 
+			// buttonPlus
+			// 
+			this.buttonPlus.Location = new System.Drawing.Point(176, 144);
+			this.buttonPlus.Name = "buttonPlus";
+			this.buttonPlus.Size = new System.Drawing.Size(30, 21);
+			this.buttonPlus.TabIndex = 9;
+			this.buttonPlus.Text = "+";
+			this.buttonPlus.UseVisualStyleBackColor = true;
+			this.buttonPlus.Click += new System.EventHandler(this.ButtonPlusClick);
+			// 
+			// buttonDiff
+			// 
+			this.buttonDiff.Location = new System.Drawing.Point(212, 144);
+			this.buttonDiff.Name = "buttonDiff";
+			this.buttonDiff.Size = new System.Drawing.Size(30, 21);
+			this.buttonDiff.TabIndex = 10;
+			this.buttonDiff.Text = "-";
+			this.buttonDiff.UseVisualStyleBackColor = true;
+			this.buttonDiff.Click += new System.EventHandler(this.ButtonDiffClick);
+			// 
+			// buttonSave
+			// 
+			this.buttonSave.Location = new System.Drawing.Point(78, 207);
+			this.buttonSave.Name = "buttonSave";
+			this.buttonSave.Size = new System.Drawing.Size(194, 42);
+			this.buttonSave.TabIndex = 11;
+			this.buttonSave.Text = "Save As";
+			this.buttonSave.UseVisualStyleBackColor = true;
+			this.buttonSave.Click += new System.EventHandler(this.ButtonSaveClick);
+			// 
+			// checkBoxCase
+			// 
+			this.checkBoxCase.Location = new System.Drawing.Point(227, 12);
+			this.checkBoxCase.Name = "checkBoxCase";
+			this.checkBoxCase.Size = new System.Drawing.Size(30, 42);
+			this.checkBoxCase.TabIndex = 12;
+			this.checkBoxCase.Text = "C";
+			this.checkBoxCase.UseVisualStyleBackColor = true;
+			// 
+			// checkBoxTranspose
+			// 
+			this.checkBoxTranspose.Location = new System.Drawing.Point(28, 207);
+			this.checkBoxTranspose.Name = "checkBoxTranspose";
+			this.checkBoxTranspose.Size = new System.Drawing.Size(30, 42);
+			this.checkBoxTranspose.TabIndex = 13;
+			this.checkBoxTranspose.Text = "T";
+			this.checkBoxTranspose.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.checkBoxTranspose);
+			this.Controls.Add(this.checkBoxCase);
+			this.Controls.Add(this.buttonSave);
+			this.Controls.Add(this.buttonDiff);
+			this.Controls.Add(this.buttonPlus);
+			this.Controls.Add(this.buttonExcVal);
+			this.Controls.Add(this.buttonIncVal);
+			this.Controls.Add(this.buttonExcLbl);
+			this.Controls.Add(this.buttonIncLbl);
+			this.Controls.Add(this.buttonExcCol);
+			this.Controls.Add(this.buttonIncCol);
+			this.Controls.Add(this.buttonPreCol);
+			this.Controls.Add(this.buttonPreRow);
+			this.Controls.Add(this.buttonOpen);
+			this.Name = "MainForm";
+			this.Text = "DataReductionInstrument";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ DataReductionInstrument/MainForm.cs
@@ -0,0 +1,272 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: USER
+ * Date: 2021/6/19
+ * Time: 上午 11:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace DataReductionInstrument
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string filePath;
+		string strIncCol;
+		string strExcCol;
+		string strIncLbl;
+		string strExcLbl;
+		string strIncVal;
+		string strExcVal;
+		string[] colnames;
+		bool[] colSelector;
+		bool[] rowSelector;
+		bool opDiff;
+		bool multOp;
+		string[] keepColNames;
+		int previewLines;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			keepColNames = ConfigurationManager.AppSettings["KeepColNames"].Split(',');
+			previewLines = Convert.ToInt32(ConfigurationManager.AppSettings["PreviewLines"]);
+		}
+		
+		void ButtonOpenClick(object sender, EventArgs e)
+		{
+			using (var openFileDialog = new OpenFileDialog()) {
+				openFileDialog.InitialDirectory = Application.StartupPath;
+				openFileDialog.Filter = "csv files (*.csv)|*.csv";
+				openFileDialog.RestoreDirectory = true;
+				
+				if (openFileDialog.ShowDialog() == DialogResult.OK) {
+					filePath = openFileDialog.FileName;
+				}
+			}
+			
+			colnames = File.ReadLines(filePath).First().Split(',');
+			colSelector = new bool[colnames.Length];
+			rowSelector = new bool[File.ReadLines(filePath).Count()];
+		}
+		
+		Regex GetIncRegex(string s)
+		{
+			s = string.IsNullOrEmpty(s) ? "(?=)" : s;
+			return checkBoxCase.Checked ? new Regex(s, RegexOptions.Compiled) : new Regex(s, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+		}
+		
+		Regex GetExcRegex(string s)
+		{
+			s = string.IsNullOrEmpty(s) ? "(?!)" : s;
+			return checkBoxCase.Checked ? new Regex(s, RegexOptions.Compiled) : new Regex(s, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+		}
+		
+		void SetFontBold(Control btn, bool bold)
+		{
+			btn.Font = new Font(btn.Font, bold ? FontStyle.Bold : FontStyle.Regular);
+		}
+		
+		void ButtonIncColClick(object sender, EventArgs e)
+		{
+			strIncCol = Interaction.InputBox("Inclusive regex for column name");
+			SetFontBold(buttonIncCol, !string.IsNullOrEmpty(strIncCol));
+		}
+		
+		void ButtonExcColClick(object sender, EventArgs e)
+		{
+			strExcCol = Interaction.InputBox("Exclusive regex for column name");
+			SetFontBold(buttonExcCol, !string.IsNullOrEmpty(strExcCol));
+		}
+		
+		bool[] _GetColSelector()
+		{
+			if (string.IsNullOrEmpty(strIncCol) && string.IsNullOrEmpty(strExcCol))
+			if (multOp)
+				return new bool[colSelector.Length];
+			else
+				return Enumerable.Repeat(true, colSelector.Length).ToArray();
+			var incRe = GetIncRegex(strIncCol);
+			var excRe = GetExcRegex(strExcCol);
+			return colnames.Select(o => incRe.IsMatch(o) && !excRe.IsMatch(o)).ToArray();
+		}
+		
+		bool[] GetColSelector()
+		{
+			var selector = opDiff ? _GetColSelector().Select((o, i) => !o && colSelector[i]).ToArray() : _GetColSelector().Select((o, i) => o || colSelector[i]).ToArray();
+			return selector.Select((o, i) => keepColNames.Contains(colnames[i]) || o).ToArray();
+		}
+		
+		void ButtonPreColClick(object sender, EventArgs e)
+		{
+			var selector = GetColSelector();
+			var selected = colnames.Where((o, i) => selector[i]).ToArray();
+			if (selected.Length > previewLines * 2)
+				MessageBox.Show(string.Join("\r\n", selected.Take(previewLines).Concat(new string[]{ "..." }).Concat(selected.Skip(selected.Length - previewLines)).ToArray()), "Selected columns");
+			else
+				MessageBox.Show(string.Join("\r\n", selected), "Selected columns");
+		}
+		
+		void ButtonIncLblClick(object sender, EventArgs e)
+		{
+			strIncLbl = Interaction.InputBox("Inclusive regex for data label");
+			SetFontBold(buttonIncLbl, !string.IsNullOrEmpty(strIncLbl));
+		}
+		
+		void ButtonExcLblClick(object sender, EventArgs e)
+		{
+			strExcLbl = Interaction.InputBox("Exclusive regex for data label");
+			SetFontBold(buttonExcLbl, !string.IsNullOrEmpty(strExcLbl));
+		}
+		
+		void ButtonIncValClick(object sender, EventArgs e)
+		{
+			strIncVal = Interaction.InputBox("Inclusive regex for data value");
+			SetFontBold(buttonIncVal, !string.IsNullOrEmpty(strIncVal));
+		}
+		
+		void ButtonExcValClick(object sender, EventArgs e)
+		{
+			strExcVal = Interaction.InputBox("Exclusive regex for data value");
+			SetFontBold(buttonExcVal, !string.IsNullOrEmpty(strExcVal));
+		}
+		
+		bool[] GetLblSelector()
+		{
+			var incRe = GetIncRegex(strIncLbl);
+			var excRe = GetExcRegex(strExcLbl);
+			return colnames.Select(o => incRe.IsMatch(o) && !excRe.IsMatch(o)).ToArray();
+		}
+		
+		bool[] _GetRowSelector()
+		{
+			if (string.IsNullOrEmpty(strIncVal) && string.IsNullOrEmpty(strExcVal))
+			if (multOp)
+				return new bool[rowSelector.Length];
+			else
+				return Enumerable.Repeat(true, rowSelector.Length).ToArray();
+			var lblSelector = GetLblSelector();
+			var incRe = GetIncRegex(strIncVal);
+			var excRe = GetExcRegex(strExcVal);
+			return File.ReadLines(filePath).Select(s => s.Split(',').Where((o, i) => lblSelector[i]).Any(o => incRe.IsMatch(o) && !excRe.IsMatch(o))).ToArray();
+		}
+		
+		bool[] GetRowSelector()
+		{
+			var selector = opDiff ? _GetRowSelector().Select((o, i) => !o && rowSelector[i]).ToArray() : _GetRowSelector().Select((o, i) => o || rowSelector[i]).ToArray();
+			selector[0] = true;
+			return selector;
+		}
+		
+		void ButtonPreRowClick(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(strIncLbl) || !string.IsNullOrEmpty(strExcLbl) || !string.IsNullOrEmpty(strIncVal) || !string.IsNullOrEmpty(strExcVal)) {
+				var _selector = GetLblSelector();
+				var _selected = colnames.Where((o, i) => _selector[i]).ToArray();
+				if (_selected.Length > previewLines * 2)
+					MessageBox.Show(string.Join("\r\n", _selected.Take(previewLines).Concat(new string[]{ "..." }).Concat(_selected.Skip(_selected.Length - previewLines)).ToArray()), "Selected data labels");
+				else
+					MessageBox.Show(string.Join("\r\n", _selected), "Selected data labels");
+			}
+			var selector = GetRowSelector();
+			var selected = selector.Count(o => o);
+			MessageBox.Show(string.Format("{0} out of {1} rows selected", selected - 1, selector.Length - 1), "Selected rows");
+		}
+		
+		void ResetControls()
+		{
+			strIncCol = null;
+			strExcCol = null;
+			strIncLbl = null;
+			strExcLbl = null;
+			strIncVal = null;
+			strExcVal = null;
+			SetFontBold(buttonIncCol, false);
+			SetFontBold(buttonExcCol, false);
+			SetFontBold(buttonIncLbl, false);
+			SetFontBold(buttonExcLbl, false);
+			SetFontBold(buttonIncVal, false);
+			SetFontBold(buttonExcVal, false);
+			SetFontBold(buttonPlus, false);
+			SetFontBold(buttonDiff, false);
+		}
+		
+		void ButtonPlusClick(object sender, EventArgs e)
+		{
+			colSelector = GetColSelector();
+			rowSelector = GetRowSelector();
+			ResetControls();
+			multOp = true;
+			opDiff = false;
+			SetFontBold(buttonPlus, true);
+		}
+		
+		void ButtonDiffClick(object sender, EventArgs e)
+		{
+			colSelector = GetColSelector();
+			rowSelector = GetRowSelector();
+			ResetControls();
+			multOp = true;
+			opDiff = true;
+			SetFontBold(buttonDiff, true);
+		}
+		
+		void SaveNormalForm(string savePath)
+		{
+			var _colSelector = GetColSelector();
+			var _rowSelector = GetRowSelector();
+			
+			using (var sw = new StreamWriter(savePath)) {
+				File.ReadLines(filePath).Where((o, i) => _rowSelector[i]).Select(s => {
+					sw.WriteLine(string.Join(",", s.Split(',').Where((o, i) => _colSelector[i]).ToArray()));
+					return true;
+				}).ToArray();
+			}
+		}
+		
+		void SaveTransposed(string savePath)
+		{
+			var _colSelector = GetColSelector();
+			var _rowSelector = GetRowSelector();
+			
+			var data = File.ReadLines(filePath).Where((o, i) => _rowSelector[i]).Select(s => s.Split(',').Where((o, i) => _colSelector[i]).ToArray()).ToArray();
+			
+			using (var sw = new StreamWriter(savePath)) {
+				var ncol = _colSelector.Count(o => o);
+				for (int i = 0; i < ncol; i++)
+					sw.WriteLine(string.Join(",", data.Select(o => o[i]).ToArray()));
+			}
+		}
+		
+		void ButtonSaveClick(object sender, EventArgs e)
+		{
+			using (var saveFileDialog = new SaveFileDialog()) {
+				saveFileDialog.InitialDirectory = Application.StartupPath;
+				saveFileDialog.Filter = "csv files (*.csv)|*.csv";
+				saveFileDialog.RestoreDirectory = true;
+				
+				if (saveFileDialog.ShowDialog() == DialogResult.OK)
+				if (!checkBoxTranspose.Checked)
+					SaveNormalForm(saveFileDialog.FileName);
+				else
+					SaveTransposed(saveFileDialog.FileName);
+			}
+		}
+	}
+}
